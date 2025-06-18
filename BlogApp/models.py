a0e560e54d70efce336463d37c5edbf535a16465
tinu <tinuunni000@gmail.com>
2025-06-18 09:57:56 +0530

    half

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -18,31 +18,37 @@ from django.contrib.auth.models import User
 
 #     def __str__(self):
 #         return '{}'.format(self.username)
+ROLE_CHOICES = [
+        ('user', 'User'),
+        ('admin', 'Admin'),
+                    ]
 
-class Userprofile(models.Model):
-    username=models.CharField(max_length=200)
+class Usertable(models.Model):
+   
+    username=models.CharField(max_length=200,unique=True)
     firstname=models.CharField(max_length=200)
     lastname=models.CharField(max_length=200)
     email=models.CharField(max_length=200)
     contact = models.CharField(max_length=20, blank=True, null=True)
     password=models.CharField(max_length=200)
-    password1=models.CharField(max_length=200)
     image=models.ImageField(upload_to='blog_media')
+    role = models.CharField(max_length=10,choices=ROLE_CHOICES)
 
     def __str__(self):
         return '{}'.format(self.username)
     
 
 class logintable(models.Model):
+    
     username=models.CharField(max_length=200)
     password=models.CharField(max_length=200)
-    password1=models.CharField(max_length=200)
-    type=models.CharField(max_length=200)
+    
 
     def __str__(self):
         return '{}'.format(self.username)
 
 class Blog(models.Model):
+    owner = models.ForeignKey(logintable, on_delete=models.CASCADE)  
     title = models.CharField(max_length=200)
     description = models.TextField()
     image=models.ImageField(upload_to='blog_media')
@@ -57,8 +63,8 @@ class Blog(models.Model):
 
 
 class Comment(models.Model):
-    blog = models.ForeignKey(Blog, related_name='comments', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE)
+    user = models.ForeignKey('logintable', on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
